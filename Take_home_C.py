83808fa452793294b5f35b1bb5a63137c2b183f9
tekshteint <36021918+tekshteint@users.noreply.github.com>
2021-01-21 18:48:58 -0800

    Update Take_home_C.py

--- Take_home_C.py
+++ Take_home_C.py
@@ -33,18 +33,22 @@ print("Sorted list3 is:",list3)
 #Part P
 list5=[list1,list2]
 print("list5 is:",list5)
-list5e=str(list5)
-print("list5 contains 4 at position:",list5.index("4"))
-#
+list5s=str(list5)
+count=0
+print("Within list5, there are two lists. List1 and List2.")
+for i in range(len(list5[0])):
+    if list5[0][i]!=4:
+        count+=1
+        print("In index[0] (list1) there are no instances of 4.")
+        break
+    elif list5[0][i]==4:
+        print("In index[0] (list1) there are ",count," instances of 4.")
+count=0
+for i in range(len(list5[1])):
+    if list5[1][i]==4:
+        count+=1
+        print("In index[1] (list2) there is",count, "instance of 4 at position",list5[1].index(4))
 
-
-
-
-
-
-
-
-"""
 print("\n")
 #Second Script- Bit Operators
 print("Onto the second script")
@@ -75,11 +79,11 @@ while placeHolder != 0:
         placeHolder=0
     else:
         binChoice
-"""
+        
 """
 Execution Results
-
 Script 1:
+
 list3 is: [1, 3, 5, 1, 2, 3, 4]
 list3 contains 3: True
 list3 contains 2 3's
@@ -91,8 +95,11 @@ slice of list3 is: [1, 2, 3]
 length of list3 is: 6
 max value of list3 is: 5
 Sorted list3 is: [1, 1, 2, 3, 4, 5]
-list5 is: ([1, 3, 5], [1, 2, 3, 4])
-list5 at position 4 is: 1
+list5 is: [[1, 3, 5], [1, 2, 3, 4]]
+Within list5, there are two lists. List1 and List2.
+In index[0] (list1) there are no instances of 4.
+In index[1] (list2) there is 1 instance of 4 at position 3
+
 
 
 Script 2:
@@ -105,8 +112,6 @@ Binary of b is: 0b1110
 Please select either a (9) or b (14) to convert to binary
 A
 9 converted to binary is: 1 0 0 1
-
-
 Onto the second script
 Binary of a is: 0b1001
 Binary of b is: 0b1110
@@ -115,8 +120,6 @@ Binary of b is: 0b1110
 Please select either a (9) or b (14) to convert to binary
 a
 9 converted to binary is: 1 0 0 1
-
-
 Onto the second script
 Binary of a is: 0b1001
 Binary of b is: 0b1110
@@ -125,7 +128,6 @@ Binary of b is: 0b1110
 Please select either a (9) or b (14) to convert to binary
 B
 14 converted to binary is: 1 1 1 0
-
 Onto the second script
 Binary of a is: 0b1001
 Binary of b is: 0b1110
@@ -134,8 +136,6 @@ Binary of b is: 0b1110
 Please select either a (9) or b (14) to convert to binary
 b
 14 converted to binary is: 1 1 1 0
-
-
 Onto the second script
 Binary of a is: 0b1001
 Binary of b is: 0b1110
